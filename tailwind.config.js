207b77300e5d1c523f80db2c558d604a292cdbbe
sajadanjidani <sajadanjiidanii@gmail.com>
2025-07-24 16:25:09 +0330

    add animation in project

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,70 @@ module.exports = {
         danaRegular : ["dana regular","sans-serif"],
         danaThin : ["dana thin","sans-serif"],
       },
+      animation: {
+        Skills90 : "Skills90 5s ease",
+        Skills80 : "Skills80 5s ease",
+        Skills75 : "Skills75 5s ease",
+        OpenMenu : "OpenMenu 2s ease",
+        CloseMenu : "CloseMenu 2s ease",
+        OpenCover : "OpenCover 2s ease",
+        CloseCover : "CloseCover 2s ease",
+        utb : "utb 2s ease",
+        btu : "btu 2s ease",
+        rtl : "rtl 2s ease",
+        ltr : "ltr 2s ease",
+        onoff : "onoff 3s ease",
+      },
+      keyframes: {
+        Skills90 : {
+          "0%" : {width: "0%"},
+          "100%" : {width: "90%"},
+        },
+        Skills80 : {
+          "0%" : {width: "0%"},
+          "100%" : {width: "80%"},
+        },  
+        Skills75 : {
+          "0%" : {width: "0%"},
+          "100%" : {width: "75%"},
+        },
+        OpenMenu:{
+          "0%":{transform:"translateX(100%)"},
+          "100%":{transform:"translateX(0%)"}
+        },
+        CloseMenu:{
+          "0%":{transform:"translateX(0%)"},
+          "100%":{transform:"translateX(100%)"}
+        },
+        OpenCover:{
+          "0%":{transform:"translateX(-100%)"},
+          "100%":{transform:"translateX(0%)"}
+        },
+        CloseCover:{
+          "0%":{transform:"translateX(0%)"},
+          "100%":{transform:"translateX(-100%)"}
+        },
+        onoff:{
+          "0%" : {opacity : "0"},
+          "100%" : {opacity : "100"},
+        },
+        utb:{
+          "0%":{transform:"translateY(-200%)"},
+          "100%":{transform:"translateY(0%)"}
+        },
+        btu:{
+          "0%":{transform:"translateY(100%)"},
+          "100%":{transform:"translateY(0%)"}
+        },
+        ltr:{
+          "0%":{transform:"translateX(-100%)"},
+          "100%":{transform:"translateX(0%)"}
+        },
+        rtl:{
+          "0%":{transform:"translateX(200%)"},
+          "100%":{transform:"translateX(0%)"},
+        }
+      }
     },
   },
   plugins: [
