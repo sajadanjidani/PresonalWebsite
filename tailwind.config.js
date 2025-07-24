ec8d1c9c451f2c011d23edf5508aaeda82a76590
sajadanjidani <sajadanjiidanii@gmail.com>
2025-07-24 16:23:18 +0330

    add font file

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,17 @@ module.exports = {
           DEFAULT : '100%',
         },
       },
+      fontFamily : {
+        danaBlack : ["dana black","sans-serif"],
+        danaBold : ["dana bold","sans-serif"],
+        danaDemibold : ["dana demibold","sans-serif"],
+        danaExtrabold : ["dana extrabold","sans-serif"],
+        danaExtralight : ["dana extralight","sans-serif"],
+        danaLight : ["dana light","sans-serif"],
+        danaMedium : ["dana medium","sans-serif"],
+        danaRegular : ["dana regular","sans-serif"],
+        danaThin : ["dana thin","sans-serif"],
+      },
     },
   },
   plugins: [
