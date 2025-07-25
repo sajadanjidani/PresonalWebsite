d37bdc4560d3f85260420eef3d9fe7537f366ea3
sajadanjidani <sajadanjiidanii@gmail.com>
2025-07-25 22:29:18 +0330

    coding PortfolioSection component

--- src/Component/PortfolioSection/PortfolioItem.jsx
+++ src/Component/PortfolioSection/PortfolioItem.jsx
@@ -2,7 +2,7 @@ import { IoIosLink } from "react-icons/io";
 
 const PortfolioItem = ({imageSrc , title , status}) => {
     return(
-        <div className="relative w-full h-full rounded-xl bg-lightBgSection cursor-pointer overflow-hidden group font-danaRegular">
+        <div className="relative w-full h-52 md:h-full rounded-xl bg-lightBgSection cursor-pointer overflow-hidden group font-danaRegular">
             {status ? (
                 <>
                     <img alt="protfolioImage" src={imageSrc} className="absolute w-full h-full top-0 right-0"/>
