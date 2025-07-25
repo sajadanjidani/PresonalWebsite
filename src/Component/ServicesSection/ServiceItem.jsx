072e3d1c3e3e106227dcbccbf96cf11bba9160d1
sajadanjidani <sajadanjiidanii@gmail.com>
2025-07-25 21:00:14 +0330

    coding ServicesSection

--- src/Component/ServicesSection/ServiceItem.jsx
+++ src/Component/ServicesSection/ServiceItem.jsx
@@ -1,16 +1,16 @@
-const ServiceItem = (icon , title , description) => {
+const ServiceItem = ({icon , title , description}) => {
     return(
-        <div className="w-full h-full px-7 rounded-lg text-customeWhite bg-darkBgSection">
+        <div className="relative mt-7 md:mt-0 w-full h-[450px] md:py-0 md:h-full px-7 rounded-lg text-lightBgSection bg-darkBgSection">
             {/* iconbox */}
-            <div className="w-28 h-28 mt-6 mx-auto bg-red-600">
+            <div className="flex justify-center items-center w-28 h-28 mt-6 mx-auto child:size-20 child:text-lightBgSection">
                 {icon}
             </div>
             {/* title */}
-            <h1 className="mt-3 text-center">{title}</h1>
+            <h1 className="text-2xl font-bold text-center">{title}</h1>
             {/* description */}
-            <p className="leading-8 text-justify group-hover:text-cWhite transition-all duration-500 tracking-tighter">{description}</p>
+            <p className="leading-8 text-justify group-hover:text-cWhite transition-all duration-500 tracking-tighter mt-5 md:mt-2">{description}</p>
             {/* btn */}
-            <a href="/#" className="block mt-4 text-center border-2 py-2 w-36 mx-auto rounded-lg hover:border-themColor hover:text-themColor">درخواست</a>
+            <a href="/#" className="absolute bottom-5 left-0 right-0 block mt-4 text-center border-2 border-lightBgSection py-2 w-36 mx-auto rounded-lg">درخواست</a>
         </div>
     )
 }
--- src/Component/ServicesSection/ServicesSection.jsx
+++ src/Component/ServicesSection/ServicesSection.jsx
@@ -1,6 +1,6 @@
 const ServicesSection = ({children}) => {
     return(
-        <div className="flex gap-10 w-[94%] h-[80%] mt-5 py-5 px-10 mx-auto">
+        <div className="gird md:flex md:gap-10 md:w-[94%] h-[80%] mt-5 py-5 px-10 mx-auto">
             {children}
         </div>
     )
