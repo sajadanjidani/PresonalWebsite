789304d559b00cbabd201c1df7a4867cea75fa6d
sajadanjidani <sajadanjiidanii@gmail.com>
2025-07-25 23:20:57 +0330

    coding responsive

--- src/Component/HomePage/HomePage.jsx
+++ src/Component/HomePage/HomePage.jsx
@@ -92,6 +92,7 @@ const HomePage = () => {
                 <TitleSection Title='ارتباط با' bgColor={darkBgSection} />
                 <FormSection />
             </Section>
+            {/* Footer */}
         </>
     )
 }
--- src/Component/ServicesSection/ServicesSection.jsx
+++ src/Component/ServicesSection/ServicesSection.jsx
@@ -1,6 +1,6 @@
 const ServicesSection = ({children}) => {
     return(
-        <div className="gird md:flex md:gap-10 md:w-[94%] h-[80%] mt-5 py-5 px-10 mx-auto">
+        <div className="gird md:flex md:gap-10 md:w-[94%] h-[80%] mt-0 md:mt-6 pt-1 pb-5 px-5 md:px-10 mx-auto">
             {children}
         </div>
     )
