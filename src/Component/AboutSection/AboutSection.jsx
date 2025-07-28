4f700a1e66940aadfbdecf394350c7265ac06144
sajadanjidani <sajadanjiidanii@gmail.com>
2025-07-28 17:49:56 +0330

    install react-intersection-observer

--- src/Component/AboutSection/AboutSection.jsx
+++ src/Component/AboutSection/AboutSection.jsx
@@ -1,28 +1,29 @@
 import { useState, useEffect } from "react";
+import { useInView } from "react-intersection-observer";
 
 const AboutSection = () => {
   const fullText = `
 عاشق دنیای کدها هستم و تلاش می‌کنم تا با توسعه وب‌سایت‌های خلاقانه، تجربه کاربری بی‌نظیری رو برای مخاطبان رقم بزنم. من سجاد انجیدنی هستم، یه برنامه نویس فرانت اند که با استفاده از HTML ، CSS  و JS، رابط‌های کاربری جذاب و کاربرپسند رو طراحی و پیاده‌سازی می‌کنم. [BR]
 در طول این دو سال، تجربه طراحی و توسعه پروژه وب‌سایت رو داشتم که از جمله اون‌ها می‌تونم به وب‌سایت شخصی خودم و وب‌سایت تیم گرین وب اشاره کنم. در حال حاضر دانشجوی رشته مهندسی نرم افزار در دانشگاه ملی مهارت سمنان هستم و همواره به دنبال یادگیری و پیشرفت در حوزه کاری خودم هستم . [BR]
 من همیشه به دنبال یادگیری و پیشرفت در حوزه کاری خودم هستم و از همکاری با افراد خلاق و با انگیزه استقبال می‌کنم. اگر به دنبال یه برنامه نویس فرانت اند برای پروژه خودتون هستید، خوشحال می‌شم با شما همکاری کنم. می‌تونید از طریق فرم تماس با من در ارتباط باشید .
-`;
-
-  // تبدیل [BR] به \n\n برای فاصله بین پاراگراف‌ها
+  `;
   const cleanedText = fullText.replace(/\[BR\]/g, "\n");
 
   const [visibleLength, setVisibleLength] = useState(0);
+  const { ref, inView } = useInView({ threshold: 0.4, triggerOnce: true });
 
   useEffect(() => {
-    if (visibleLength < cleanedText.length) {
-      const timer = setTimeout(() => {
+    let timer;
+    if (inView && visibleLength < cleanedText.length) {
+      timer = setTimeout(() => {
         setVisibleLength((prev) => prev + 1);
-      }, 10); // سرعت نمایش هر کاراکتر
-      return () => clearTimeout(timer);
+      }, 10);
     }
-  }, [visibleLength]);
+    return () => clearTimeout(timer);
+  }, [inView, visibleLength]);
 
   return (
-    <div className="grid lg:flex h-full lg:mx-12 my-6">
+    <div className="grid lg:flex h-full lg:mx-12 my-6" ref={ref}>
       {/* imagebox */}
       <div className="hidden lg:block w-80 h-5/6 mt-12 mr-20 rounded-t-full bg-darkBgSection order-2 lg:order-1"></div>
 
--- src/Component/ServicesSection/ServiceItem.jsx
+++ src/Component/ServicesSection/ServiceItem.jsx
@@ -1,17 +1,28 @@
-const ServiceItem = ({icon , title , description}) => {
+import { motion } from "framer-motion";
+import { useState } from "react";
+
+const ServiceItem = ({icon , title , description , customeClass}) => {
+    const [isVisible, setIsVisible] = useState(false);
+    
     return(
-        <div className="relative mt-7 lg:mt-0 w-full h-[500px] lg:h-full lg:py-0  px-7 rounded-lg text-lightBgSection bg-darkBgSection child:duration-500 cursor-pointer group">
-            {/* iconbox */}
-            <div className="flex justify-center items-center w-28 h-28 mt-6 mx-auto child:size-20 child:text-lightBgSection group-hover:child:text-themColor child:duration-500">
-                {icon}
-            </div>
-            {/* title */}
-            <h1 className="text-2xl font-bold text-center group-hover:text-themColor">{title}</h1>
-            {/* description */}
-            <p className="leading-8 text-justify group-hover:text-customeWhite transition-all tracking-tighter mt-5 lg:mt-2">{description}</p>
-            {/* btn */}
-            <a href="/#" className="absolute bottom-5 left-0 right-0 block mt-4 text-center border-2 border-lightBgSection py-2 w-36 mx-auto rounded-lg group-hover:border-customeWhite group-hover:text-customeWhite hover:bg-lightBgSection">درخواست</a>
+        <motion.div
+      onViewportEnter={() => setIsVisible(true)}
+      viewport={{ once: true, amount: 0.3 }}
+      className={`relative mt-7 lg:mt-0 w-full h-[500px] lg:h-full lg:py-0  px-7 rounded-lg text-lightBgSection bg-darkBgSection child:duration-500 cursor-pointer group ${
+        isVisible ? customeClass : ""
+      }`}
+    >
+        {/* iconbox */}
+        <div className="flex justify-center items-center w-28 h-28 mt-6 mx-auto child:size-20 child:text-lightBgSection group-hover:child:text-themColor child:duration-500">
+            {icon}
         </div>
+        {/* title */}
+        <h1 className="text-2xl font-bold text-center group-hover:text-themColor">{title}</h1>
+        {/* description */}
+        <p className="leading-8 text-justify group-hover:text-customeWhite transition-all tracking-tighter mt-5 lg:mt-2">{description}</p>
+            {/* btn */}
+        <a href="/#" className="absolute bottom-5 left-0 right-0 block mt-4 text-center border-2 border-lightBgSection py-2 w-36 mx-auto rounded-lg group-hover:border-customeWhite group-hover:text-customeWhite hover:bg-lightBgSection">درخواست</a>
+    </motion.div>
     )
 }
 
