993b62293bb56f0d50bf15b0e37c38ca8fae28cc
sajadanjidani <sajadanjiidanii@gmail.com>
2025-08-03 18:01:24 +0330

    coding link in navbar

--- src/Component/FooterSection/FooterSection.jsx
+++ src/Component/FooterSection/FooterSection.jsx
@@ -4,7 +4,7 @@ import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import { FiPhone } from "react-icons/fi";
 import { MdOutlineMail } from "react-icons/md";
-
+import { Link } from 'react-scroll';
 
 const FooterSection = () => {
     return(
@@ -12,12 +12,12 @@ const FooterSection = () => {
             <div className='mx-auto'>
                 <p className='text-center text-4xl font-bold text-lightBgSection hover:text-themColor cursor-pointer duration-500'>TC</p>
                 <ul className='grid grid-cols-3 grid-rows-2 md:flex mt-5 md:mt-10 gap-5 md:gap-10 text-customeWhite child-hover:text-themColor cursor-pointer child:text-center child:duration-500'>
-                    <li>خانه</li>
-                    <li>درباره من</li>
-                    <li>مهارت</li>
-                    <li>خدمات</li>
-                    <li>نمونه کار</li>
-                    <li>ارتباط</li>
+                    <li><Link to='Home' smooth={true} duration={1600}>خانه</Link></li>
+                    <li><Link to='AboutMe' smooth={true} duration={1400}>درباره من</Link></li>
+                    <li><Link to='Skills' smooth={true} duration={1200}>مهارت</Link></li>
+                    <li><Link to='Services' smooth={true} duration={1000}>خدمات</Link></li>
+                    <li><Link to='Portfolio' smooth={true} duration={800}>نمونه کار</Link></li>
+                    <li><Link to='Contact' smooth={true} duration={500}>ارتباط</Link></li>
                 </ul>
                 <ul className='flex mt-10 gap-10 justify-center'>
                     <li><TelegramIcon className='text-themColor hover:text-customeWhite cursor-pointer child:duration-500' style={{width : '40px', height : '40px'}}/></li>
--- src/Component/NavbarSection/NavbarSection.jsx
+++ src/Component/NavbarSection/NavbarSection.jsx
@@ -1,6 +1,6 @@
 import MenuIcon from '@mui/icons-material/Menu';
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-scroll';
 
 const NavbarSection = () => {
 
@@ -15,27 +15,27 @@ const NavbarSection = () => {
         <div className="flex items-center justify-between w-[94%] mx-auto md:w-full h-14 px-7 rounded-lg bg-lightBgSection animate-utb">
             <span className="text-2xl font-bold text-darkBgSection hover:cursor-pointer hover:text-themColor duration-500">TC</span>
             <ul className="hidden md:flex md:gap-9 lg:gap-12 font-danaMedium text-customeWhite child-hover:cursor-pointer child-hover:text-themColor child:duration-500">
-                <li><Link to='#Home'>خانه</Link></li>
-                <li><Link to='#Contact'>درباره من</Link></li>
-                <li><Link to='#AboutMe'>مهارت</Link></li>
-                <li><Link to='#Services'>خدمات</Link></li>
-                <li><Link to='#Portfolio'>نمونه کار</Link></li>
-                <li><Link to='#Skills'>ارتباط</Link></li>
+                <li><Link to='Home' smooth={true} duration={500}>خانه</Link></li>
+                <li><Link to='AboutMe' smooth={true} duration={800}>درباره من</Link></li>
+                <li><Link to='Skills' smooth={true} duration={1000}>مهارت</Link></li>
+                <li><Link to='Services' smooth={true} duration={1200}>خدمات</Link></li>
+                <li><Link to='Portfolio' smooth={true} duration={1400}>نمونه کار</Link></li>
+                <li><Link to='Contact' smooth={true} duration={1600}>ارتباط</Link></li>
             </ul>
             <button className="hidden md:block text-themColor hover:text-customeWhite duration-500">درخواست همکاری</button>
             <button onClick={() => toggleMenu()} className='md:hidden hover:text-themColor'><MenuIcon /></button>
 
             {/* menu mobile */}
             {menuStatus ? (
-                <div className='fixed w-full h-full top-0 right-0'>
+                <div className='fixed w-full h-full top-0 right-0 z-50'>
                     <div className='absolute w-3/5 h-full top-0 right-0 py-8 px-7 bg-darkBgSection'>
                         <ul className="grid gap-5 text-customeWhite child-hover:cursor-pointer child-hover:text-themColor child:text-lg child:duration-500">
-                            <li><Link to='#Home'>خانه</Link></li>
-                            <li><Link to='#AboutMe'>مهارت</Link></li>
-                            <li><Link to='#Skills'>ارتباط</Link></li>
-                            <li><Link to='#Services'>خدمات</Link></li>
-                            <li><Link to='#Portfolio'>نمونه کار</Link></li>
-                            <li><Link to='#Contact'>درباره من</Link></li>
+                            <li><Link to='Home' smooth={true} duration={500}>خانه</Link></li>
+                            <li><Link to='Skills' smooth={true} duration={1000}>مهارت</Link></li>
+                            <li><Link to='Contact' smooth={true} duration={1600}>ارتباط</Link></li>
+                            <li><Link to='Services' smooth={true} duration={1200}>خدمات</Link></li>
+                            <li><Link to='Portfolio' smooth={true} duration={1400}>نمونه کار</Link></li>
+                            <li><Link to='AboutMe' smooth={true} duration={800}>درباره من</Link></li>
                         </ul>
                     </div>
                     <div onClick={() => toggleMenu()} className='absolute w-2/5 h-full top-0 left-0 opacity-80 bg-lightBgSection'></div>
